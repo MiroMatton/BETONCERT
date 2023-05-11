6c61fec8b660cf603718d3ea194991975e64e290
MiroMatton <miro.matton@student.odisee.be>
2023-05-11 19:31:15 +0200

    ✨ logica certificaten lijst generen

--- server/api.go
+++ server/api.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"runtime"
 )
 
 type Company struct {
@@ -15,6 +18,7 @@ type Company struct {
 	CountryId          int                `json:"CountryId"`
 	Tel                string             `json:"Tel"`
 	VAT                string             `json:"VAT"`
+	CategoryId         int                `json:"categoryId"`
 	ProductionEntities []ProductionEntity `json:"ProductionEntities"`
 }
 
@@ -37,15 +41,18 @@ func api(url string) []Data {
 
 	req, _ := http.NewRequest("GET", url, nil)
 
-	res, _ := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
 
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 
 	var data []Data
 
-	err := json.Unmarshal(body, &data)
-	if err != nil {
+	e := json.Unmarshal(body, &data)
+	if e != nil {
 		panic(err)
 	}
 
@@ -54,4 +61,58 @@ func api(url string) []Data {
 	// }
 
 	return data
+}
+
+type Entity struct {
+	Id          int           `json:"Id"`
+	Name        string        `json:"Name"`
+	Certificate []Certificate `json:"Certificates"`
+}
+
+type Certificate struct {
+	ID                       int     `bson:"ID"`
+	Product                  string  `bson:"Product"`
+	CertificateNumber        string  `bson:"CertificateNumber"`
+	Standard                 string  `bson:"Standard"`
+	TechnicalSpecification   *string `bson:"TechnicalSpecification"`
+	CertificateReport        int     `bson:"CertificateReport"`
+	SectorID                 int     `bson:"SectorId"`
+	StatusID                 int     `bson:"StatusId"`
+	NotLicensed              bool    `bson:"NotLicensed"`
+	NotLicensedMessage       *string `bson:"NotLicensedMessage"`
+	CertificationStatusID    int     `bson:"CertificationStatusId"`
+	CertificationNotLicensed bool    `bson:"CertificationNotLicensed"`
+	Suspended                bool    `bson:"Suspended"`
+}
+
+func certApi(companyId int, categoryId int) []Entity {
+
+	defer func() {
+		if r := recover(); r != nil {
+			buf := make([]byte, 1<<16)
+			runtime.Stack(buf, true)
+			log.Printf("panic: %v\n%s", r, buf)
+		}
+	}()
+
+	url := fmt.Sprintf("https://extranet.be-cert.be/api/HomePage/GetProductsTreeBranchForCompanyAndSector?languageIsoCode=en&treeFilters={%%22companyId%%22:%d,%%22sectorId%%22:%d,%%22certificationType%%22:%%22*%%22}", companyId, categoryId)
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var certificate []Entity
+
+	e := json.Unmarshal(body, &certificate)
+	if e != nil {
+		panic(err)
+	}
+
+	return certificate
 }
--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -27,29 +28,108 @@ func main() {
 	}
 	defer client.Disconnect(ctx)
 
+	// company(client, ctx)
+	processProductionEntities(client, ctx)
+	// getCollection(client, ctx)
+}
+
+func company(client *mongo.Client, ctx context.Context) {
 	url := "https://extranet.be-cert.be/api/HomePage/GetCertificateHoldersTree?languageIsoCode=en&treeFilters=%7B%22certificationType%22%3A%22*%22%7D"
 	data := api(url)
 
-	productsCollection := client.Database("demo").Collection("companies")
+	productsCollection := client.Database("demo").Collection("companiesTest")
 
-	for _, companyData := range data {
-		for _, company := range companyData.Company {
+	var err error // declare an err variable of type error
+
+	for _, category := range data {
+		for _, company := range category.Company {
+			company.CategoryId = category.Id
+
+			fmt.Println("Company:", company.Name, "was added")
 			_, err = productsCollection.InsertOne(ctx, company)
 			if err != nil {
 				panic(err)
 			}
-			fmt.Println("company: ", company, "was added")
 		}
 	}
+}
 
-	// catsCollection := client.Database("demo").Collection("cats")
-	// cursor, err := catsCollection.Find(ctx, bson.M{})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// var cats []bson.M
-	// if err = cursor.All(ctx, &cats); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(cats)
+func getCollection(client *mongo.Client, ctx context.Context) ([]bson.M, error) {
+	var results []bson.M
+
+	// Access the "companiesTest" collection from the database
+	companiesTest := client.Database("your_database_name").Collection("companiesTest")
+
+	// Passing nil as the filter matches all documents in the collection
+	cursor, err := companiesTest.Find(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	// Loop through the cursor and append each document to the results slice
+	for cursor.Next(ctx) {
+		var result bson.M
+		if err := cursor.Decode(&result); err != nil {
+			return nil, err
+		}
+		results = append(results, result)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	// Return the results slice containing all the documents from the collection
+	return results, nil
+}
+
+func processProductionEntities(client *mongo.Client, ctx context.Context) {
+	// Get the companies collection from the "demo" database
+	companiesCollection := client.Database("demo").Collection("companiesTest")
+
+	// Get the certificates collection from the "demo" database
+	certificatesCollection := client.Database("demo").Collection("certificates")
+
+	// Find all documents in the collection
+	cursor, err := companiesCollection.Find(ctx, bson.M{})
+	if err != nil {
+		panic(err)
+	}
+	defer cursor.Close(ctx)
+
+	// Decode the documents into an array of Company structs
+	var companies []Company
+	if err = cursor.All(ctx, &companies); err != nil {
+		panic(err)
+	}
+
+	for _, company := range companies {
+		var certs []interface{}
+
+		data := certApi(company.Id, company.CategoryId)
+
+		if data == nil || len(data) == 0 {
+			continue
+		}
+
+		for _, doc := range data {
+			bsonData, err := bson.Marshal(doc)
+			if err != nil {
+				fmt.Println("skipping invalid certificate:", err)
+				continue
+			}
+			certs = append(certs, bsonData)
+		}
+
+		if certs == nil || len(certs) == 0 {
+			continue
+		}
+
+		res, err := certificatesCollection.InsertMany(ctx, certs)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Inserted", len(res.InsertedIDs), "certificates")
+	}
 }
