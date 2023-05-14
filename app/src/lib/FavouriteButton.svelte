9fff777a4d9723602ebfeda68a83f65ba1aa148f
miro-matton <miro.matton@endare.com>
2023-05-14 12:55:28 +0200

    ✨ favourite button added + refactoring

---
+++ app/src/lib/FavouriteButton.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+    export let Id: number;
+    export let IsFavourite: boolean;
+
+    async function toggleFavourite() {
+        IsFavourite = !IsFavourite; // toggle the value of IsFavourite
+
+        try {
+            // send PUT request to update the favourite status on the server
+            const url = `http://localhost:8080/api/certificates/${Id}`;
+            const response = await fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify({ IsFavourite }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors'
+            });
+
+            // check if request was successful
+            if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData.message);
+            } else {
+            console.error(response.statusText);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+</script>
+
+<img src={`src/assets/${IsFavourite ? 'favourite' : 'unFavourite'}.svg`} alt="favourite button" on:click={toggleFavourite} />
+
+<style lang="scss">
+    img {
+        margin: 0 0.5rem;
+        cursor: pointer;
+    }
+</style>
