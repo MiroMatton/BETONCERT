324eb9fc87825e4bd36defa74625748136f43149
miro-matton <miro.matton@endare.com>
2023-05-14 17:45:07 +0200

    🐛 cors error fix

--- server/go.mod
+++ server/go.mod
@@ -5,16 +5,19 @@ go 1.19
 require go.mongodb.org/mongo-driver v1.11.1
 
 require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
 require (
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/cors v1.9.0
 	github.com/tkanos/gonfig v0.0.0-20210106201359-53e13348de2f
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
--- server/router.go
+++ server/router.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -16,122 +17,116 @@ import (
 )
 
 func corsMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-        w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token")
-        if r.Method == "OPTIONS" {
-            return
-        }
-        next.ServeHTTP(w, r)
-    })
+	headersOk := handlers.AllowedHeaders([]string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "Authorization", "X-CSRF-Token", "X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{"http://localhost:5173"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	return handlers.CORS(headersOk, originsOk, methodsOk)(next)
 }
 
 func server(client *mongo.Client, ctx context.Context) {
-    r := mux.NewRouter()
-
-	r.Use(corsMiddleware)
+	r := mux.NewRouter()
 
 	r.HandleFunc("/api/certificates", getCertificatesHandler(client, ctx)).Methods("GET")
-    r.HandleFunc("/api/certificates/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/api/certificates/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
 		toggleFavouriteHandler(w, r, client)
-	}).Methods("PUT")	
+	}).Methods("PUT")
 
-    log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", corsMiddleware(r)))
 }
 
 func getCertificatesHandler(client *mongo.Client, ctx context.Context) func(http.ResponseWriter, *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
-        // Extract page and perPage parameters from query string
-        pageStr := r.URL.Query().Get("page")
-        favourites, _ := getUserFavorites(client, ctx, "645ff9c78f9b2d306a6d52ff")
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Extract page and perPage parameters from query string
+		pageStr := r.URL.Query().Get("page")
+		favourites, _ := getUserFavorites(client, ctx, "645ff9c78f9b2d306a6d52ff")
 
-        // Convert parameters to integers (default to 1 and 25 if not provided)
-        page, _ := strconv.Atoi(pageStr)
-        if page < 1 {
-            page = 1
-        }
+		// Convert parameters to integers (default to 1 and 25 if not provided)
+		page, _ := strconv.Atoi(pageStr)
+		if page < 1 {
+			page = 1
+		}
 
-        // Get data from MongoDB
-        results, err := getCertificates(client, ctx, page, favourites)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
+		// Get data from MongoDB
+		results, err := getCertificates(client, ctx, page, favourites)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-        // Encode data as JSON and write to response
-        if err := json.NewEncoder(w).Encode(results); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-    }
+		// Encode data as JSON and write to response
+		if err := json.NewEncoder(w).Encode(results); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
 }
 
 func toggleFavouriteHandler(w http.ResponseWriter, r *http.Request, client *mongo.Client) {
-    // Extract certificate ID from URL path
-    id, err := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/api/certificates/"))
-    if err != nil {
-        http.Error(w, "Invalid certificate ID", http.StatusBadRequest)
-        return
-    }
+	// Extract certificate ID from URL path
+	id, err := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/api/certificates/"))
+	if err != nil {
+		http.Error(w, "Invalid certificate ID", http.StatusBadRequest)
+		return
+	}
 
-    // Extract isFavourite value from request body
-    var reqBody struct {
-        IsFavourite bool `json:"isFavourite"`
-    }
-    if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
-        http.Error(w, "Invalid request body", http.StatusBadRequest)
-        return
-    }
+	// Extract isFavourite value from request body
+	var reqBody struct {
+		IsFavourite bool `json:"isFavourite"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
 
-    // Update favorite status for user in MongoDB
-    userCollection := client.Database("demo").Collection("users")
+	// Update favorite status for user in MongoDB
+	userCollection := client.Database("demo").Collection("users")
 	objectID, err := primitive.ObjectIDFromHex("645ff9c78f9b2d306a6d52ff")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    filter := bson.M{"_id": objectID}
-    update := bson.M{}
-    if reqBody.IsFavourite {
-        update = bson.M{"$addToSet": bson.M{"favoriteCertificates": id}}
-    } else {
-        update = bson.M{"$pull": bson.M{"favoriteCertificates": id}}
-    }
-    _, err = userCollection.UpdateOne(context.TODO(), filter, update)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	filter := bson.M{"_id": objectID}
+	update := bson.M{}
+	if reqBody.IsFavourite {
+		update = bson.M{"$addToSet": bson.M{"favoriteCertificates": id}}
+	} else {
+		update = bson.M{"$pull": bson.M{"favoriteCertificates": id}}
+	}
+	_, err = userCollection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    w.WriteHeader(http.StatusOK)
-    if err := json.NewEncoder(w).Encode(map[string]interface{}{
-        "message": fmt.Sprintf("Favourite status for certificate %d updated for user %s", reqBody.IsFavourite, id),
-    }); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": fmt.Sprintf("Favourite status for certificate %d updated for user %s", reqBody.IsFavourite, id),
+	}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func getUserFavorites(client *mongo.Client, ctx context.Context, id string) ([]int, error) {
-    collection := client.Database("demo").Collection("users")
+	collection := client.Database("demo").Collection("users")
 
-    objectId, err := primitive.ObjectIDFromHex(id)
-    if err != nil {
-        return nil, err
-    }
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
 
-    filter := bson.M{"_id": objectId}
+	filter := bson.M{"_id": objectId}
 
-    var result struct {
-        FavoriteCertificates []int `bson:"favoriteCertificates"`
-    }
+	var result struct {
+		FavoriteCertificates []int `bson:"favoriteCertificates"`
+	}
 
-    err = collection.FindOne(ctx, filter).Decode(&result)
-    if err != nil {
-        return nil, err
-    }
+	err = collection.FindOne(ctx, filter).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
 
-    return result.FavoriteCertificates, nil
+	return result.FavoriteCertificates, nil
 }
