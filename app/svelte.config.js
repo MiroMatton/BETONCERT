7d788aaa45275ec836b8513affbd0e991c59011f
MiroMatton <miro.matton@student.odisee.be>
2023-09-25 14:17:03 +0200

    🚑 wrong config var

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -16,13 +16,15 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    server: {
-      cors: {
-          origin: ' http://localhost:8080',
-          methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      },
   },
-},
+
+  // Move the server configuration outside of the kit object
+  server: {
+    cors: {
+      origin: 'http://localhost:8080',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+  },
 };
 
 export default config;
